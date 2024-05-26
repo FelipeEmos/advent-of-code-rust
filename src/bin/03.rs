74df20ec7809d78d6822a2287f53b2a3bd5b4039
Felipe Emos <felipe.emos.computacao@gmail.com>
2024-05-25 21:46:24 -0300

    feat: solve 3 pt 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(3);
 
 trait MatrixSafeAccess<T> {
@@ -23,12 +25,18 @@ impl<T> MatrixSafeAccess<T> for Vec<Vec<T>> {
     }
 }
 
+struct MatrixItem {
+    item: char,
+    row: isize,
+    col: isize,
+}
+
 fn get_neighbors(
     matrix: &Vec<Vec<char>>,
     i: isize,
     initial_j: isize,
     final_j: isize,
-) -> Vec<&char> {
+) -> Vec<MatrixItem> {
     let left = vec![
         (i - 1, initial_j - 1),
         (i, initial_j - 1),
@@ -50,8 +58,21 @@ fn get_neighbors(
 
     return locations
         .iter()
-        .filter_map(|(row, col)| matrix.safe_get(*row, *col))
+        .filter_map(|(row, col)| match matrix.safe_get(*row, *col) {
+            Some(c) => Some(MatrixItem {
+                item: *c,
+                row: *row,
+                col: *col,
+            }),
+            None => None,
+        })
         .collect();
+}
+
+fn has_part_neighbor(neighbors: &Vec<MatrixItem>) -> bool {
+    neighbors
+        .iter()
+        .any(|MatrixItem { item, .. }| !item.is_numeric() && *item != '.')
 }
 
 fn parse_matrix(input: &str) -> Vec<Vec<char>> {
@@ -73,57 +94,93 @@ pub fn part_one(input: &str) -> Option<u32> {
                 if str_initial_j.is_none() {
                     str_initial_j = Some(j);
                 }
-            } else {
-                if let Some(initial_j) = str_initial_j {
-                    let neighbors =
-                        get_neighbors(&matrix, i as isize, initial_j as isize, j as isize - 1);
-
-                    let is_special_part_number = neighbors
-                        .iter()
-                        .filter(|&c| !c.is_numeric() && **c != '.')
-                        .count()
-                        > 0;
-
-                    if is_special_part_number {
-                        part_numbers.push(number_str.parse::<u32>().unwrap())
-                    }
-
-                    str_initial_j = None;
-                    number_str.clear();
-                }
             }
-        }
 
-        if let Some(initial_j) = str_initial_j {
-            let neighbors = get_neighbors(
-                &matrix,
-                i as isize,
-                initial_j as isize,
-                matrix[i].len() as isize - 1,
-            );
+            if str_initial_j.is_some() {
+                let end_of_line = j == matrix[i].len() - 1;
+                let end_of_word = !matrix[i][j].is_numeric();
 
-            let is_special_part_number = neighbors
-                .iter()
-                .filter(|&c| !c.is_numeric() && **c != '.')
-                .count()
-                > 0;
+                if !end_of_line && !end_of_word {
+                    continue;
+                }
 
-            if is_special_part_number {
-                part_numbers.push(number_str.parse::<u32>().unwrap())
+                let initial_j = str_initial_j.unwrap();
+                let final_j = if end_of_line { j } else { j - 1 };
+
+                let neighbors =
+                    get_neighbors(&matrix, i as isize, initial_j as isize, final_j as isize);
+
+                if has_part_neighbor(&neighbors) {
+                    part_numbers.push(number_str.parse::<u32>().unwrap())
+                }
+
+                str_initial_j = None;
+                number_str.clear();
             }
         }
     }
 
     let sum: u32 = part_numbers.iter().sum();
 
-    // println!("PARTS: {:?}", part_numbers);
-    // println!("SUM: {:?}", sum);
-
     Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let matrix = parse_matrix(input);
+    let mut gear_numbers_sets: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
+
+    for i in 0..matrix.len() {
+        let mut number_str = String::new();
+        let mut str_initial_j: Option<usize> = None;
+
+        for j in 0..matrix[i].len() {
+            if matrix[i][j].is_numeric() {
+                number_str.push(matrix[i][j]);
+
+                if str_initial_j.is_none() {
+                    str_initial_j = Some(j);
+                }
+            }
+
+            if str_initial_j.is_some() {
+                let end_of_line = j == matrix[i].len() - 1;
+                let end_of_word = !matrix[i][j].is_numeric();
+
+                if !end_of_line && !end_of_word {
+                    continue;
+                }
+
+                let initial_j = str_initial_j.unwrap();
+                let final_j = if end_of_line { j } else { j - 1 };
+
+                let neighbors =
+                    get_neighbors(&matrix, i as isize, initial_j as isize, final_j as isize);
+
+                let current_number = number_str.parse::<u32>().unwrap();
+
+                neighbors
+                    .iter()
+                    .filter(|MatrixItem { item, .. }| *item == '*')
+                    .for_each(|MatrixItem { row, col, .. }| {
+                        gear_numbers_sets
+                            .entry((*row as usize, *col as usize))
+                            .and_modify(|v| v.push(current_number))
+                            .or_insert(vec![current_number]);
+                    });
+
+                str_initial_j = None;
+                number_str.clear();
+            }
+        }
+    }
+
+    let sum: u32 = gear_numbers_sets
+        .values()
+        .filter(|v| v.len() > 1)
+        .map(|v| v.iter().fold(1, |acc, x| acc * x))
+        .sum();
+
+    Some(sum)
 }
 
 #[cfg(test)]
